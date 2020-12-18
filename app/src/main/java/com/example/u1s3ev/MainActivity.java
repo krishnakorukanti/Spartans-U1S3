2a8fe0c7930bf5d35e02702f7342be0bb69d73f4
krishnakorukanti <crishnakorukanti@gmail.com>
2020-12-19 00:50:33 +0530

    Both Layouts of Evaluation

---
+++ app/src/main/java/com/example/u1s3ev/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.u1s3ev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // change R.layout.insta_layout to run the other layout
+        
+    }
+}
